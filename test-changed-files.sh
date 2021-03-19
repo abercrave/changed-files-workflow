5aed5b1759b2bab89ae0a24c72559c55ed95836d
Ben Evans <ben.evans@chghealthcare.com>
2021-03-19 15:02:04 -0600

    Add second watched file

--- test-changed-files.sh
+++ test-changed-files.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 WATCHED_DIRECTORY=watched-directory
-FOUND_WATCHED_FILES=0
 
-for f in `git diff --name-only HEAD~1 HEAD`; do 
-  if [[ $f = ${WATCHED_DIRECTORY}/* ]]; then
+for FILE in `git diff --name-only HEAD~1 HEAD`; do 
+  if [[ $FILE = ${WATCHED_DIRECTORY}/* ]]; then
     FOUND_WATCHED_FILES=1
-    echo $f
+    echo $FILE
     break
   fi
 done
 
-echo $FOUND_WATCHED_FILES
+if [[ -n $FOUND_WATCHED_FILES ]]; then
+  echo $FOUND_WATCHED_FILES
+fi
