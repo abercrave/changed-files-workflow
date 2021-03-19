b992610044a5495fb3c1a189c7af34f4fc3cd4ab
Ben Evans <ben.evans@chghealthcare.com>
2021-03-19 14:53:58 -0600

    Add test file to watched dir

---
+++ test-changed-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WATCHED_DIRECTORY=watched-directory
+FOUND_WATCHED_FILES=0
+
+for f in `git diff --name-only HEAD~1 HEAD`; do 
+  if [[ $f = ${WATCHED_DIRECTORY}/* ]]; then
+    FOUND_WATCHED_FILES=1
+    break
+  fi
+done
+
+echo $FOUND_WATCHED_FILES
